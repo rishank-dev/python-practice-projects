81dda2b8d87282018d498b827c70167798dfd386
Rishank Gupta <gauravpoonam1710@gmail.com>
2026-02-27 22:32:05 +0530

    Improved calculator with input validation and zero division check

--- calculator.py
+++ calculator.py
@@ -3,7 +3,20 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-print("Sum =", num1+num2)
-print("Difference =", num1-num2)
-print("Product =", num1*num2)
-print("Division =", num1/num2)
+print("\nOperations :-\n1 → Addition\n2 → Subtraction\n3 → Multiplication\n4 → Division\n")
+
+choice = int(input("Enter the number corresponding to the operation : "))
+
+if choice==1:
+    print("Sum =", num1+num2)
+elif choice==2:
+    print("Difference =", num1-num2)
+elif choice==3:
+    print("Product =", num1*num2)
+elif choice==4:
+    if num2==0:
+        print("Division by 0 is not possible.\nZeroDivisionError")
+    else:
+        print("Division =", num1/num2)
+else:
+    print("Invalid Choice")
