36767e7741211733cafacbaf862487e18a7e229d
Rishank Gupta <gauravpoonam1710@gmail.com>
2026-02-27 21:55:47 +0530

    Initial commit: added simple calculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# Calculator
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Sum =", num1+num2)
+print("Difference =", num1-num2)
+print("Product =", num1*num2)
+print("Division =", num1/num2)
