85bad70831003812cda8e3bfb5422ce6bd2d856f
Rishank Gupta <gauravpoonam1710@gmail.com>
2026-03-03 21:49:09 +0530

    feat: upgrade Number Guessing Game with difficulty levels and replay system
    Enhanced the Number Guessing Game with structured improvements:
    
    - Added difficulty selection (Easy, Medium, Hard)
    - Implemented fixed attempt limit with proper loop control
    - Added intelligent hint feedback (Very Close / Close / Too High / Too Low)
    - Included replay functionality
    - Improved input validation and error handling
    - Refactored logic for cleaner structure and readability
    
    Focused on maintaining beginner-friendly design while improving game experience and code quality.

--- number_guess_game.py
+++ number_guess_game.py
@@ -1,25 +1,58 @@
 # Number Guessing Game 
 import random
 
+
 # Showing the Intro
 def show_intro():
-    print("Welcome to Number Guessing Game! \nI am thinking of a number from 0 to 100 (both inclusive). \nTry to guess it! \n")
+    print("Welcome to Number Guessing Game!\n")
+
+
+# Setting the difficulty
+def choose_difficulty():
+    max_easy = 50
+    max_medium = 100
+    max_hard = 500
+    print("\nChoose a difficulty level:\n1 → Easy (from 0 to 50) \n2 → Medium (from 0 to 100) \n3 → Hard (from 0 to 500) \n")
+    while True:
+        try:
+            level = int(input("Enter level(1/2/3) : "))
+        except ValueError:
+            print("Invalid Input! \nTry entering a valid number! \n")
+            continue
+            
+        if level in [1,2,3]:
+            if level==1:
+                print(f"\nI am thinking of a number from 0 to {max_easy} (both inclusive). \nTry guessing it. \nYou will have a total of 7 attempts. \n")
+                return max_easy
+            elif level==2:
+                print(f"\nI am thinking of a number from 0 to {max_medium} (both inclusive). \nTry guessing it. \nYou will have a total of 7 attempts. \n")
+                return max_medium
+            else:
+                print(f"\nI am thinking of a number from 0 to {max_hard} (both inclusive). \nTry guessing it. \nYou will have a total of 7 attempts. \n")
+                return max_hard
+        else:
+            print("Enter a valid number!! \n")
+
 
 # Generating a random number to be guessed
 def generate_num():
-    computer_num = random.randint(0,100)
-    return computer_num
+    max = choose_difficulty()
+    computer_num = random.randint(0,max)
+    return computer_num , max
+
 
 # Playing the game
 def play():
     attempts = 0
-    num = generate_num()
-    while True:
+    max_attempts = 7
+    num , max_range = generate_num()
+    
+    while attempts<max_attempts:
         attempts += 1
         try:
-            user_num = int(input(f"Attempt number : {attempts} \nEnter a number: "))
-            if not (0<=user_num<=100):
-                print("Enter the number in the valid range.")
+            user_num = int(input(f"Remaining attempts : {(max_attempts+ 1) - attempts} \nEnter a number: "))
+            if not (0<=user_num<=max_range):
+                print("Enter the number in the valid range.\n")
                 continue
         except ValueError:
             print("Enter a valid number!!")
@@ -44,12 +77,31 @@ def play():
                 print("The number you enter is HIGHER than the correct number, but you are CLOSE.\n")
             else:
                 print("The number entered by you is TOO HIGH\n")
-                
+        
+    else:
+            print(f"You run out of the total attempts!!!! \nThe correct number was {num}.")
+
+
+# Ask to play again
+def play_again():
+    print("\nDo you want to play again?")
+    while True:
+        replay = input("Enter 'yes' to play again and 'no' to quit : ")
+        if replay.lower() == 'yes':
+            return True
+        elif replay.lower() == 'no':
+            return False
+        else:
+            print("Invalid Input! \n")
+
 
 # Declaring the main loop in which game is run
 def main_loop():
     show_intro()
-    play()
+    while True:
+        play()
+        if not play_again():
+            break
 
 # Running the main loop
 main_loop()
